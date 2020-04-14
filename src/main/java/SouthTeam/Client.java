fa2e46cb5571eb6e60b44710daf992d6d7ac168f
Praveena1989 <pravisiva757@gmail.com>
2020-04-14 13:03:47 +0200

    Update Client class

--- src/main/java/SouthTeam/Client.java
+++ src/main/java/SouthTeam/Client.java
@@ -23,7 +23,7 @@ public class Client {
 	public Client(String name, String phoneNumber, String email) {
 
 
-		if(name == null || name.isEmpty()) {
+		if(name.isEmpty()) {
 			throw new IllegalArgumentException("name missing!");
 		}
 		else {
@@ -64,39 +64,29 @@ public class Client {
 	
 	@Override
 	public int hashCode() {
+		
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		result = prime * result + name.hashCode();
+		result = prime * result + phoneNumber.hashCode();
+		result = prime * result + email.hashCode();
+		
 		return result;
 	}
 	
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Client other = (Client) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (phoneNumber == null) {
-			if (other.phoneNumber != null)
-				return false;
-		} else if (!phoneNumber.equals(other.phoneNumber))
-			return false;
-		return true;
+
+		if(obj instanceof Client) {
+			Client that = (Client)obj;
+			if(this.name.equals(that.name) 
+				&& this.phoneNumber.equals(that.phoneNumber) 
+					&& this.email == that.email ) {
+				return true;
+			}
+		}
+		return false;
+		
 	}
 
 	// To check the valid email id
